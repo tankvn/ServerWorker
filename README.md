6ac2651bee7a354337639b6e52aa8743edf6327d
TAn <tanvd357@gmail.com>
2024-12-04 13:35:57 +0700

    NLog Install

--- README.md
+++ README.md
@@ -15,6 +15,23 @@ dotnet run
 dotnet publish --output "C:\custom\publish\directory"
 ```
 
+## Installing NLog
+
+Install NLog nuget package for Windows Service .NET framework
+- NLog is a logging platform for .NET with rich log routing and management capabilities.
+- NLog supports traditional logging, structured logging and the combination of both.
+
+Standard install
+1. Install the latest NLog version 5.3.4 from [NuGet](https://www.nuget.org/packages/NLog/).
+   - dotnet add package NLog --version 5.3.4
+2. Setup initial NLog.config xml-file.
+   - Choose "Add" > "New Item..."
+   - In the "Add New Item" dialog, search for "XML File" or "XML" and select it.
+   - Name the file NLog.config and click the "Add" button.
+3. Set "Copy To Output Directory" to "Copy if newer" for the NLog.config.
+
+That's it, you can now compile and run your application and it will be able to use NLog.
+
 ## Install the service
 
 ```bash
--- ServerWorker/Worker.cs
+++ ServerWorker/Worker.cs
@@ -2,11 +2,12 @@ namespace ServerWorker;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    // Create a static logger field
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
     public Worker(ILogger<Worker> logger)
     {
-        _logger = logger;
+        //_logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -15,7 +16,7 @@ public class Worker : BackgroundService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                Logger.Info("Worker running at: {time}", DateTimeOffset.Now);
 
                 await Task.Delay(3000, stoppingToken);
             }
@@ -27,7 +28,7 @@ public class Worker : BackgroundService
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "{Message}", ex.Message);
+            Logger.Error(GetExceptionInfo(ex, "Worker.cs"));
 
             // Terminates this process and returns an exit code to the operating system.
             // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
@@ -39,5 +40,20 @@ public class Worker : BackgroundService
             // recovery options, we need to terminate the process with a non-zero exit code.
             Environment.Exit(1);
         }
+    }
+
+    private string GetExceptionInfo(Exception ex, string className)
+    {
+        var lineNumber = 0;
+        const string lineSearch = ":line ";
+        var index = ex.StackTrace.LastIndexOf(lineSearch);
+        if (index != -1)
+        {
+            var lineNumberText = ex.StackTrace.Substring(index + lineSearch.Length);
+            if (int.TryParse(lineNumberText, out lineNumber))
+            {
+            }
+        }
+        return $"{className}:line {lineNumber} | {ex.GetType()}: {ex.Message}";
     }
 }
